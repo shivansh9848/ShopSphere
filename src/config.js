eefe0e324bdb073a97fe999aaafcad62fe19463e
Shivansh9848 <104058319+shivansh9848@users.noreply.github.com>
2025-03-26 10:55:19 +0530

    Rename project to react-shopsphere and configure environment for development

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = "https://shopsphere-backend-j6eo.onrender.com/";
+export const API_BASE_URL = process.env.NODE_ENV === "development" ? "/" : "https://shopsphere-backend-j6eo.onrender.com";
