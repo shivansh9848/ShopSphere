19bba057c52d44c03527c8c1b2fb0abd421b66cf
Shivansh9848 <104058319+shivansh9848@users.noreply.github.com>
2025-03-28 19:04:04 +0530

    Update environment variable for development mode and adjust API base URL configuration

--- src/config.js
+++ src/config.js
@@ -1,1 +1,4 @@
-export const API_BASE_URL = process.env.NODE_ENV === "development" ? "/" : "https://shopsphere-backend-j6eo.onrender.com";
+export const API_BASE_URL =
+  process.env.REACT_APP_MODE === "development"
+    ? "/"
+    : "https://shopsphere-backend-j6eo.onrender.com";
